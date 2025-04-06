131e87eecc1db32b02eec6633f76c512e54919e3
Naomi Inoferio <156651776+njinoferio@users.noreply.github.com>
2025-04-06 23:18:04 +0800

    Update Skybox.h

--- GDGRAP Playbook/Classes/Skybox.h
+++ GDGRAP Playbook/Classes/Skybox.h
@@ -8,21 +8,17 @@
 
 class Skybox {
 public:
-    // Constructor and Destructor
     Skybox(const std::vector<std::string>& faces);
     ~Skybox();
 
-    // Method to render the skybox
     void Draw(const glm::mat4& view, const glm::mat4& projection);
 
 private:
-    GLuint skyboxVAO, skyboxVBO, cubemapTexture, skyboxEBO;  // Added skyboxEBO declaration
+    GLuint skyboxVAO, skyboxVBO, cubemapTexture, skyboxEBO; 
     GLuint skyboxShader;
 
-    // Method to load cubemap textures
     GLuint LoadCubemap(const std::vector<std::string>& faces);
 
-    // Optional: Method to load shaders (if you need to implement it separately)
     GLuint LoadShader(const char* vertexPath, const char* fragmentPath);
 };
 
